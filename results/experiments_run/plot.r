a539667cb288269c4b5ea2e66b2059dcf5dc52fa
aomond <antoine.omond@imt-atlantique.fr>
2026-05-06 17:21:32 +0200

    Updated plot script

--- results/experiments_run/plot.r
+++ results/experiments_run/plot.r
@@ -5,13 +5,12 @@ library(rlang)
 library(patchwork)
 library(stringr)
 options(dplyr.print_max = 1e9, pillar.width = Inf)
-baseline_mapfunc <- function(lvls) { return(gsub("\\..+", "", lvls)) }
+baseline_mapfunc <- function(lvls) { return(gsub("(C|V|L)\\.", "\\1 | ", lvls)) }
 baseline_f <- function(df_input) {
 	df_input <- df_input %>%
 		filter(clock_source %in% c("PLL", "XOSC", "ROSC")) %>%
 		filter(vreg_output %in% c("1.10V")) %>%
 		filter(clock_freq %in% c("150001000", "12000000", "11029000"))
-	#return(df_input)
 	return(list(df_input, c("PLL", "XOSC", "ROSC"), "baseline", baseline_mapfunc))
 }
 pll_f <- function(df_input) {
@@ -56,15 +55,8 @@ lposc_min_f <- function(df_input) {
 		filter(lposc_trim %in% c(0x000))
 	return(list(df_input, c("LPOSC 1.10V (baseline)", "LPOSC (1.00V)", "LPOSC (0.90V)", "LPOSC (0.80V) (lowest v)"), "minimums-lposc_min", baseline_mapfunc))
 }
-clock_colors <- c(
-	"PLL"   = "black",
-	"XOSC"  = "purple",
-	"ROSC"  = "brown",
-	"LPOSC" = "dark green"
-)
-y_energy_offset <- 20
-x_clock_freq <- 33
-clock_freq_unit <- "MHz"
+MHz <- 1000000
+kHz <- 1000
 folder = ""
 name <- paste(folder, "results", sep="")
 df <- read.csv(paste(name, ".csv", sep=""))
@@ -85,38 +77,31 @@ for(expe in c(baseline_f, pll_f, rosc_f, xosc_f, lposc_dft_f, lposc_max_f, lposc
 	level_names <- res[[2]]
 	pdf_name <- res[[3]]
 	mapfunc <- res[[4]]
-	df_expe$clock_source <- factor(df_expe$clock_source, levels = c("PLL", "XOSC", "ROSC", "LPOSC"))
-	df_expe$vreg_output <- factor(df_expe$vreg_output, levels = c("1.10V", "1.00V", "0.90V", "0.80V"))
-
-	#df_expe <- df_expe %>% filter(iteration_num == 1)
-	#df_expe <- baseline_f(df_expe)
 
 	# power
 	power_summary <- df_expe %>%
 		group_by(clock_source, vreg_output, clock_freq) %>%
-		summarise(
-			power_median = median(power_sample, na.rm = TRUE)
-			#clock_freq = freqs[unique(legend_group)]
-		)
-	mtimestamp <- max(df_expe$current_timestamp, na.rm = TRUE)
-	df_expe$gp <- interaction(df_expe$clock_source, df_expe$vreg_output, paste(round(df_expe$clock_freq/1000000, 1), "MHz", sep=""))
+		summarise(power_median = median(power_sample, na.rm = TRUE))
+	unit <- if (max(df_expe$clock_freq, na.rm = TRUE) < MHz) "kHz" else "MHz"
+	div <- if (max(df_expe$clock_freq, na.rm = TRUE) < MHz) kHz else MHz
+	df_expe$gp <- interaction(df_expe$clock_source, df_expe$vreg_output, paste(round(df_expe$clock_freq/div, 1), unit, sep=""))
 	
 	lvls <- levels(df_expe$gp)
 	res_mapping <- setNames(mapfunc(lvls), lvls)
+	df_expe$clock_source <- factor(df_expe$clock_source, levels = c("PLL", "XOSC", "ROSC", "LPOSC"))
+	df_expe$vreg_output <- factor(df_expe$vreg_output, levels = c("1.10V", "1.00V", "0.90V", "0.80V"))
 	df_expe$gp <- factor(df_expe$gp, levels = unique(df_expe$gp[order(df_expe$clock_source, df_expe$vreg_output, -df_expe$clock_freq)]))
 	
-	#levels(df_expe$gp) <- level_names
+	mtimestamp <- max(df_expe$current_timestamp, na.rm = TRUE)
 	p1 <- ggplot(df_expe, aes(x = current_timestamp, y = power_sample, color=gp, group=gp)) +
 		geom_line(na.rm = TRUE) +
 		geom_hline(data = power_summary, aes(yintercept = power_median), linetype = "dashed") +
 		geom_label_repel(data = power_summary, aes(x=mtimestamp*1.05, y = power_median, label = paste(round(power_median,2), "mW")), hjust = "left", show.legend = FALSE, inherit.aes = FALSE, direction = "y") +
-		#geom_text(data = power_summary, aes(x=x_clock_freq, y = power_median+y_power_median_offset, label = paste(round(clock_freq/1000000, 2), clock_freq_unit)), hjust = 1.1, vjust=-0.4, show.legend = FALSE) +
-		#scale_y_continuous(limits=c(0, y_max), n.breaks=15) +
 		scale_x_continuous(expand = expansion(mult = c(0, 0.3))) +
 		scale_y_continuous(n.breaks=15) +
-		labs(fill="Processor clock:", title = "", x = "Timestamp in seconds", y = "Power usage in mW") +
-		scale_color_discrete(name = "Configuration:", labels = res_mapping)
-		#scale_color_manual(name = "Processor clock:")
+		labs(x = "Timestamp in seconds", y = "Power usage in mW") +
+		scale_color_discrete(name = "Configuration:", labels = res_mapping) + 
+		guides(color = guide_legend(nrow = 2, byrow = TRUE)) 
 
 	# energy
 	energy_consumption <- df_expe %>%
@@ -128,25 +113,16 @@ for(expe in c(baseline_f, pll_f, rosc_f, xosc_f, lposc_dft_f, lposc_max_f, lposc
 		group_by(expe_num) %>%
 		summarise(gp = gp, avg_energy = mean(energy_sample), std_energy = sd(energy_sample), .groups = "drop") %>%
 		distinct()
-
-	#write.csv(energy_consumption, "energy_consumption.csv") 
-
 	p2 <- ggplot(energy_consumption, aes(x = gp, y = avg_energy, fill=gp)) +
 		geom_bar(stat = "identity", width = 0.2) +
 		geom_errorbar(aes(ymin = avg_energy - std_energy, ymax = avg_energy + std_energy), width = 0.2) +
-		geom_text(aes(label = round(avg_energy), y = avg_energy + y_energy_offset)) +
-		labs(x = "Processor clock",
-			y = "Total energy consumption in mJ",
-			title = ""
-		) +
+		geom_text(aes(label = round(avg_energy), y = avg_energy, nudge_y = 50)) +
+		labs(x = "Processor clock", y = "Total energy consumption in mJ", title = "") +
 		scale_x_discrete(labels = res_mapping) +
-		#scale_fill_manual(name = "Processor clock") +
 		scale_fill_discrete(labels = res_mapping) +
-		
 		theme(aspect.ratio = 3/1, legend.position = "none", axis.text.x = element_text(angle = 45, size = 8, hjust = 1))
 
 	p2 <- p2 + guides(color = "none", fill = "none", linetype = "none")
-
 	combined_plot <- (p1 + p2) + 
 	plot_layout(guides = "collect") & 
 	theme(legend.position = "top")
